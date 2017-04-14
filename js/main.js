1b7c21baf010e2ad3d0c692672b1715759007256
hansel ke <hansel@openbusiness.com.sg>
2017-04-14 17:22:16 +0800

    order > pay > thanks

--- js/main.js
+++ js/main.js
@@ -321,6 +321,48 @@
 
 		}
 	};
+	var faqsAnimate = function() {
+		var faqs = $('#fh5co-faqs');
+		if ( faqs.length > 0 ) {	
+
+			faqs.waypoint( function( direction ) {
+										
+				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+
+					var sec = faqs.find('.to-animate').length,
+						sec = parseInt((sec * 200) + 400);
+
+					setTimeout(function() {
+						faqs.find('.to-animate').each(function( k ) {
+							var el = $(this);
+							
+							setTimeout ( function () {
+								el.addClass('fadeInUp animated');
+							},  k * 200, 'easeInOutExpo' );
+							
+						});
+					}, 200);
+
+					setTimeout(function() {
+						faqs.find('.to-animate-2').each(function( k ) {
+							var el = $(this);
+							
+							setTimeout ( function () {
+								el.addClass('bounceIn animated');
+							},  k * 200, 'easeInOutExpo' );
+							
+						});
+					}, sec);
+
+
+					
+					$(this.element).addClass('animated');
+						
+				}
+			} , { offset: '80%' } );
+
+		}
+	};
 
 	var aboutAnimate = function() {
 		var about = $('#fh5co-about');
@@ -494,6 +536,7 @@
 		aboutAnimate();
 		countersAnimate();
 		contactAnimate();
+		faqsAnimate();
 		
 
 	});
